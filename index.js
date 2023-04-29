a3cbfd6c957cc8e81662fc3cb40971e31f3f6a21
Abu Bokor <abubokor1066@gmail.com>
2023-04-29 16:21:31 +0600

    all done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3000;
 const catagories = require("./Data/Catagories.json");
+const news = require("./Data/News.json");
 
 app.use(cors());
 
@@ -12,6 +13,23 @@ app.get("/", (req, res) => {
 
 app.get("/catagories", (req, res) => {
   res.send(catagories);
+});
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+app.get("/catagories/:id", (req, res) => {
+  const id = req.params.id;
+  if (id == 0) {
+    res.send(news);
+  } else {
+    const categoriesNews = news.filter((n) => n.category_id === id);
+    res.send(categoriesNews);
+  }
 });
 
 app.listen(port, () => {
